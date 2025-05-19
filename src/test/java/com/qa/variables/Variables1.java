554f12302a5d8debec4e25011ece1101cb93423d
Preetamkamat <31009397+Preetamkamat@users.noreply.github.com>
2025-05-19 12:21:15 +0530

    added test1 method in Variables1.java class

--- src/test/java/com/qa/variables/Variables1.java
+++ src/test/java/com/qa/variables/Variables1.java
@@ -6,5 +6,14 @@ public class Variables1 {
 
     public static void main(String[] args) {
         System.out.println(a);
+        Variables1 var1 = new Variables1();
+        var1.test1();
+
+    }
+
+    public void test1(){
+        // local variables
+        float b = 4.5f;
+        System.out.println(b);
     }
 }
